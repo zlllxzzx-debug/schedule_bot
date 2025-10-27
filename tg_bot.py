aec406dac96b77f91080a9cc38ad934a1b16f48b
M1racalis <zlllxzzx@gmail.com>
2025-10-27 16:16:32 +0300

    новый функционал

--- tg_bot.py
+++ tg_bot.py
@@ -8,8 +8,20 @@ api_token = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(api_token)
 
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    bot.reply_to(message, "Hello World")
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Привет! Отправьте ваш WhatsApp-токен.")
 
-bot.polling()
+@bot.message_handler(func=lambda message: True)
+def save_token(message):
+    chat_id = str(message.from_user.id)
+    token = message.text.strip()
+    
+    # Сохранение токена в файл с названием id_пользователя.txt
+    with open(f"{chat_id}.txt", "w") as file:
+        file.write(token)
+    
+    bot.send_message(chat_id, "Ваш WhatsApp-токен успешно сохранён!")
+
+print("Bot is running...")
+bot.polling(non_stop=True)
