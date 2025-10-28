493d3f62a077d24565bda02a8d8c3ea7f0ce760a
M1racalis <zlllxzzx@gmail.com>
2025-10-28 17:22:50 +0300

    localized the code

--- tg_bot.py
+++ tg_bot.py
@@ -16,21 +16,23 @@ def handle_start(message):
     user_id = message.from_user.id
     user_state[user_id] = 'waiting_for_token'
     bot.send_message(message.chat.id, "Привет! Отправьте ваш WhatsApp-токен.")
-
+#
 @bot.message_handler(func=lambda message: True)
 def save_token(message):
     user_id = message.from_user.id
-    if user_state.get(user_id) == 'waiting_for_token':
-        token = message.text.strip()
-        
-        # Сохранение токена в файл с названием id_пользователя.txt
-        with open(f"{user_id}.txt", "w") as file:
-            file.write(token)
-        
-        bot.send_message(message.chat.id, "Ваш WhatsApp-токен успешно сохранён!")
-        user_state[user_id] = None  # Сбрасываем состояние пользователя
-    else:
+    if user_state.get(user_id) != 'waiting_for_token':
         bot.send_message(message.chat.id, "Я не понимаю ваше сообщение.")
+        return
+    token = message.text.strip()
+    response = save_token_to_file(user_id, token)
+    bot.send_message(message.chat.id, response)
+    user_state[user_id] = None
+
+#Функция для сохранения токена в файле
+def save_token_to_file(user_id, token):
+    with open(f"{user_id }.txt", "w") as file:
+        file.write(token)
+    return "Ваш WhatsApp-токен успешно сохранен!"
 
 print("Bot is running...")
 bot.polling(non_stop=True)
